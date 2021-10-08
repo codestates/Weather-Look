67cab72f5e076c6c531433d4ec2b04aa8842017d
HYO0 <hyo02222@gmail.com>
2021-10-08 19:06:39 +0900

    login, signup ing

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,28 @@
-import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Common/Header/Header";
 import Footer from "./components/Common/Footer/Footer";
 import Main from "./pages/Main";
 import Mypage from "./pages/Mypage";
-import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 
 function App() {
- 
-
   return (
     <div className="app">
       <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact={true} path="/">
-          <Main />
-        </Route>
-        <Route path="/mypage">
-          <Mypage />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-      </Switch>
-      <Footer />
+        <Header />
+        <Switch>
+          <Route exact={true} path="/">
+            <Main />
+          </Route>
+          <Route path="/mypage">
+            <Mypage />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- client/src/components/Common/Header/Header.js
+++ client/src/components/Common/Header/Header.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudSun } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
-import {Link} from 'react-router-dom';
-import SigninModal, { SiginModal } from '../SigninModal/SiginModal'
+import { Link } from "react-router-dom";
+import SigninModal, { SiginModal } from "../SigninModal/SiginModal";
 
 export const HeaderContainer = styled.div`
   display: flex;
@@ -11,11 +11,20 @@ export const HeaderContainer = styled.div`
   align-items: center;
   height: 4rem;
   padding: 0.5em 1em;
-  background: #afc3fa;
+  background: #2c2891;
   .logo {
     display: flex;
     align-items: center;
     padding-right: 2em;
+  }
+  .h1 {
+    color: white;
+  }
+  .Link {
+    &:hover {
+      color: red;
+    }
+    color: white;
   }
 `;
 
@@ -23,44 +32,64 @@ export const Button = styled.button`
   padding-right: 2em;
   background-color: transparent;
   border: none;
-  font-size: 1.2em;
+  font-size: 16px;
   color: white;
-  font-weight: bold;
+
   &:hover {
     color: red;
   }
 `;
 
-const Header = (props) => {
-  const [isOpenModal,setIsOpenModal] = useState(false);
+function Header() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const openModalHandler = () => {
-    setIsOpenModal(!isOpenModal)
-  }
-  {/**const [isLogin, setIsLogin] = useState(false);
+    setIsOpenModal(!isOpenModal);
+  };
+  {
+    /**\
   
 
   const handleLogin = () => {
     setIsLogin(!isLogin);
     setIsOpenModal(!isOpenModal);
-  } */}
+  } */
+  }
 
   return (
-    
     <HeaderContainer>
       <div className="logo">
         <FontAwesomeIcon icon={faCloudSun} size="3x" color="white" />
-        <h1>WeatherLook</h1>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1 className="h1">WeatherLook</h1>
+        </Link>
       </div>
       <div className="button">
-        <Link to="/mypage" style={{ textDecoration: 'none' }}>Mypage</Link>
-        <button onClick={openModalHandler}>Login</button>
-        {isOpenModal ? <SiginModal openModalHandler={openModalHandler}/> : null}
-         {/**{isLogin ? <Button>logout</Button> : <Button onClick={handleLogin}>login</Button>}
+        {/** mypage를 누르면 로그인상태일때는 마이페이지로 가고 그게 아니면 로그인 모달이 떠야한다.  */}
+        {
+          <Link
+            className="Link"
+            to="/mypage"
+            style={{ textDecoration: "none" }}
+            onClick={openModalHandler}
+          >
+            마이페이지
+          </Link>
+        }
+
+        <Button onClick={openModalHandler} className="button">
+          로그인
+        </Button>
+        {isOpenModal && !isLogin ? (
+          <>
+            <SiginModal openModalHandler={openModalHandler} />
+          </>
+        ) : null}
+        {/**{isLogin ? <Button>logout</Button> : <Button onClick={handleLogin}>login</Button>}
         {isOpenModal ? <SigninModal /> : null} */}
       </div>
     </HeaderContainer>
-    
   );
-};
+}
 
 export default Header;
